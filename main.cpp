30c552d0bd01cb52e2aa0fdc8e4b3c122dc48a62
Marlon SYmczecym <pomps.marlon@gmail.com>
2019-04-10 21:44:33 -0300

    Algoritmo

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	char nome[50];
+	char letraFind[1];
+	char letraReplace[1];
+	
+	char * pFind;
+	pFind = letraFind;
+	
+	char * pReplace;
+	pReplace = letraReplace;
+	
+	int i;
+	
+	cout << "Qual o seu nome ? ";
+	cin >> nome;
+	
+	cout << "Qual letra quer procurar ? ";
+	cin >> *pFind;
+	
+	cout << "Por qual letra deseja trocar ? ";
+	cin >> *pReplace;
+	
+	for(i = 0; i < sizeof(nome)/sizeof(int); i++){
+		if(tolower(nome[i]) == tolower(*pFind)){
+			nome[i] = *pReplace;
+		}
+	}
+	
+	cout << nome << endl;
+	
+	delete[] pFind;
+	pFind = NULL;
+	delete[] pReplace;
+	pReplace = NULL;
+}
